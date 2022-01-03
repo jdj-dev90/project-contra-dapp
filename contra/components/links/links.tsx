9f7c6101e166e4124ce60bd8d3397fdeaecdb1b8
clayton <clayton.waldock@monarcglobal.com>
2022-01-03 16:16:03 +1000

    edit acc details ui wip

--- contra/components/links/links.tsx
+++ contra/components/links/links.tsx
@@ -1,21 +1,25 @@
-import { Box, Button } from "@mantine/core";
+import { Box, Button, Title } from "@mantine/core";
 import { FC, useState } from "react";
 import ModalWrapper from "../common/modalWrapper";
 import LinkForm from "./linkForm";
 import LinksList from "./linksList";
+
 interface PropTypes {}
 
 const Links: FC<PropTypes> = () => {
   const [open, setOpen] = useState(false);
-  console.log("open", { open });
+
   return (
-    <Box>
-      <h1>links header</h1>
-      <Button onClick={() => setOpen(true)}>+ Add Link</Button>
-      <ModalWrapper open={open} setOpen={setOpen}>
-        <LinkForm setOpen={setOpen} />
-      </ModalWrapper>
-      <LinksList modalOpen={open} />
+    <Box sx={{ display: "flex", justifyContent: "center" }}>
+      <Box sx={{ display: "flex", flexDirection: "column", width: "100%" }}>
+        <Title order={2}>Account Details</Title>
+
+        <Button onClick={() => setOpen(true)}>+ Add Link</Button>
+        <ModalWrapper open={open} setOpen={setOpen}>
+          <LinkForm setOpen={setOpen} />
+        </ModalWrapper>
+        <LinksList modalOpen={open} />
+      </Box>
     </Box>
   );
 };
--- contra/pages/profile/[userId].tsx
+++ contra/pages/profile/[userId].tsx
@@ -1,28 +1,12 @@
-import { Box, Button, Divider } from "@mantine/core";
-import { useRouter } from "next/router";
+import { Box, Divider } from "@mantine/core";
 import { useContext, useEffect, useState } from "react";
 import DisplayLinkList from "../../components/common/profile/displayLinkList";
 import ProfileHeader from "../../components/common/profile/header";
 import { UserDetails } from "../../types";
-import { AppStateContext } from "../_app";
 import { gun, useMe } from "../../utils/gun";
-
-// const LinkItem: FC<{ title: string }> = ({ title }) => {
-//   return (
-//     <Box
-//       sx={{
-//         display: "flex",
-//         padding: "5px 0",
-//         border: "1px solid orange",
-//       }}
-//     >
-//       {title}
-//     </Box>
-//   );
-// };
+import { AppStateContext } from "../_app";
 
 export default function Profile() {
-  const router = useRouter();
   const [details, setDetails] = useState<UserDetails>();
   const { userId } = useContext(AppStateContext);
 
@@ -37,26 +21,29 @@ export default function Profile() {
   }, []);
   useMe("userDetails");
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 10,
-        // border: "1px solid red",
-      }}
-    >
-      <ProfileHeader
-        displayName={details?.displayName || ""}
-        username={details?.username || ""}
-        bio={details?.bio || ""}
-        privacyType={details?.privacyType || ""}
-      />
-      <Divider
-        sx={{
-          margin: "15px 10px",
-        }}
-      />
-      <DisplayLinkList />
+    <Box sx={{ display: "flex", justifyContent: "center" }}>
+      <Box sx={{ display: "flex" }}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            padding: 10,
+          }}
+        >
+          <ProfileHeader
+            displayName={details?.displayName || ""}
+            username={details?.username || ""}
+            bio={details?.bio || ""}
+            privacyType={details?.privacyType || ""}
+          />
+          <Divider
+            sx={{
+              margin: "15px 10px",
+            }}
+          />
+          <DisplayLinkList />
+        </Box>
+      </Box>
     </Box>
   );
 }
